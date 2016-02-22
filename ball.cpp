c41da95a55881c2d278416643ec7378bc8ecb217
xywang9334 <wangmcindy@hotmail.com>
2016-02-22 13:41:16 -0600

    ball object

--- ball.cpp
+++ ball.cpp
@@ -13,16 +13,23 @@ ball::ball(Ogre::String name,
 		this->radius = radius;
 		Entity *entity = sceneMgr->createEntity(name + "Node", name);
 		this->_entity = entity;
-		this->node = _rootNode->createChildSceneNode(name + "Node");
+		this->node = _rootNode->createChildSceneNode("sphereNode");
 		this->node->attachObject(_entity);
 		this->node->setPosition(initialPosition);
 		this->_entity->setCastShadows(true);
 		this->radius = entity->getBoundingRadius();
+		this->velocity = Vector3(0, 0, 0);
 	}
 
-void ball::update()
+void ball::update(const FrameEvent &evt)
 {
+	Vector3 position = this->node->getPosition();
+	this->node->translate(speed * evt.timeSinceLastFrame * velocity);
+}
 
+void ball::changeVelocity(Vector3 velocity)
+{
+	this->velocity = velocity;
 }
 
 ball::~ball()
--- ball.h
+++ ball.h
@@ -15,13 +15,29 @@ using namespace Ogre;
 class ball: public GameObject {
 private:
 	Real radius;
+	Vector3 velocity;
 public:
 	ball(Ogre::String name, Ogre::SceneManager *sceneManager, 
 		 Simulator* simulator, btScalar mass,
 		 btScalar restitution, btScalar friction,
 		 Real radius, Vector3 initialPosition);
 	~ball();
+	void changeVelocity(Vector3 velocity);
 	String material;
-	void update();
+	virtual void update(const FrameEvent &evt);
 	SceneNode *node;
+	Real speed;
+
+	inline btVector3 convertVectorToBtVector(const Vector3 &V)
+	{
+    	return btVector3(V.x, V.y, V.z);
+	}
+
+	inline Vector3 convertBtVectorToVector(const btVector3 &v)
+	{
+		return Vector3(V.x(), V.y(), V.z());
+	}
+
+
+
 };
